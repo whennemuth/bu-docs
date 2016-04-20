8317be16846b3d216adb8a29d4de35fd7fda214a
Bugs Bunny <bugs@warnerbros.com>
2016-04-20 19:18:16 -0400

    Updates to ci documentation and aws jenkins setup

--- aws/ssh_kc.sh
+++ aws/ssh_kc.sh
@@ -3,7 +3,7 @@ close=$1
 if [ $# -ne 1 ]; then
    eval `ssh-agent -s`
    ssh-add C:/Users/wrh/.ssh/ec2_rsa
-   ssh centos@ec2-52-37-253-82.us-west-2.compute.amazonaws.com
+   ssh ec2-user@ec2-52-37-253-82.us-west-2.compute.amazonaws.com
 else
    eval `ssh-agent -k`
    exit
