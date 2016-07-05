e3173f18787f69ff67e7612cca6990e67f03d2d9
Bugs Bunny <bugs@warnerbros.com>
2016-07-05 19:46:30 -0400

    Adding WhyDocker.odt

--- aws/ssh/bastion.sh
+++ aws/ssh/bastion.sh
@@ -3,7 +3,7 @@ close=$1
 if [ $# -ne 1 ]; then
    eval `ssh-agent -s`
    ssh-add C:/Users/wrh/.ssh/buaws-kuali-rsa
-   ssh -A wrh@ec2-54-172-65-242.compute-1.amazonaws.com
+   ssh -A wrh@ec2-52-205-118-191.compute-1.amazonaws.com
 else
    eval `ssh-agent -k`
    exit
