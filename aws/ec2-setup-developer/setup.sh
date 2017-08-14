f95f53047f7561735584024e53e0022414a75ae9
Bugs Bunny <bugs@warnerbros.com>
2017-08-14 01:29:24 -0400

    Finished schemaspy build (not tested)

--- aws/ec2-setup-developer/setup.sh
+++ aws/ec2-setup-developer/setup.sh
@@ -5,19 +5,33 @@
 # This tomcat instance is started in such a way as to accept remote debugging sessions
 # from anyone who is tunnelled in through port 8080
 
+BASE=/opt/kuali
+KC=$BASE/kc
+
 run() {
+
   installJava
+
   installMaven
+
   installTools
+
   installTomcat8
+
   getKuali
-  setMavenVersions /opt/kuali/kc/pom.xml
+
   getSchemaSpy
+
   getRice
+
   getApi
+
   getS2sgen
+
   configureKuali
+
   buildKuali
+
   runKuali
 }
 
@@ -64,58 +78,58 @@ installTomcat8() {
 }
 
 getKuali() {
-  local KC=/opt/kuali/kc
+  [ -z "$KC" ] && KC=/opt/kuali/kc
   if [ -f $KC/pom.xml ] ; then
     echo "Kuali codebase already exists at $KC"
     echo "Cancelling git clone."
     return 0
   fi
 
-  local GIT_HOST="$(propertyFileLookup GIT_HOST)"
-  local GIT_REPO="$(propertyFileLookup GIT_REPO)"
-  local GIT_REFSPEC="$(propertyFileLookup GIT_REFSPEC)"
-  local GIT_KEY="$(propertyFileLookup GIT_KEY)"
-  local GIT_URL=""
+  local GIT_BU_HOST="$(propertyFileLookup GIT_BU_HOST)"
+  local GIT_BU_ORG="$(propertyFileLookup GIT_BU_ORG)"
+  local GIT_BU_REPO="$(propertyFileLookup GIT_BU_REPO)"
+  local GIT_BU_REFSPEC="$(propertyFileLookup GIT_BU_REFSPEC)"
+  local GIT_BU_KEY="$(propertyFileLookup GIT_BU_KEY)"
+  local GIT_BU_URL=""
 
-  if [ -n "$GIT_KEY" ] ; then
+  if [ -n "$GIT_BU_KEY" ] ; then
     echo "Git private ssh key found. Using ssh to clone repo"
     ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts;
-    chmod 600 $GIT_KEY;
+    chmod 600 $GIT_BU_KEY;
     eval `ssh-agent -s`
-    ssh-add $GIT_KEY    
-    GIT_URL="git@$GIT_HOST:${GIT_REPO}.git"
+    ssh-add $GIT_BU_KEY    
+    GIT_BU_URL="git@$GIT_BU_HOST:${GIT_BU_ORG}/${GIT_BU_REPO}.git"
   else
     # Get the user and password. NOTE Assume the git password is an arg passed to this function, 
     # else go to the properties file for it.
-    GIT_USER="$(propertyFileLookup GIT_USER)"
-    GIT_PASSWORD="$1"
-    [ -z "$GIT_PASSWORD" ] && GIT_PASSWORD="$(propertyFileLookup GIT_PASSWORD)"
+    GIT_BU_USER="$(propertyFileLookup GIT_BU_USER)"
+    GIT_BU_PASSWORD="$1"
+    [ -z "$GIT_BU_PASSWORD" ] && GIT_BU_PASSWORD="$(propertyFileLookup GIT_BU_PASSWORD)"
 
     # Compose the full git url
-    if [ -n "$GIT_USER" ] && [ -n "$GIT_PASSWORD" ] ; then 
+    if [ -n "$GIT_BU_USER" ] && [ -n "$GIT_BU_PASSWORD" ] ; then 
       echo "No git private ssh key found. Trying http protocol with username and password"
-      GIT_URL="https://${GIT_USER}@${GIT_HOST}/${GIT_REPO}.git"
+      GIT_BU_URL="https://${GIT_BU_USER}@${GIT_BU_HOST}/${GIT_BU_ORG}/${GIT_BU_REPO}.git"
     else
-      GIT_PASSWORD=""
+      GIT_BU_PASSWORD=""
       echo "No git authentication available. Assuming public access ..."
-      GIT_URL="https://${GIT_HOST}/${GIT_REPO}.git"
+      GIT_BU_URL="https://${GIT_BU_HOST}/${GIT_BU_ORG}/${GIT_BU_REPO}.git"
     fi
   fi
 
-  # 
-  if [ -z "$GIT_PASSWORD" ] ; then
-    local CMD="git clone --no-checkout $GIT_URL $KC"
+  if [ -z "$GIT_BU_PASSWORD" ] ; then
+    local CMD="git clone --no-checkout $GIT_BU_URL $KC"
     echo $CMD
   else
-    local CMD="echo \"$GIT_PASSWORD\" | git clone --no-checkout $GIT_URL $KC <&0"
-    echo "echo \"*******\" | git clone --no-checkout $GIT_URL $KC <&0"
+    local CMD="echo \"$GIT_BU_PASSWORD\" | git clone --no-checkout $GIT_BU_URL $KC <&0"
+    echo "echo \"*******\" | git clone --no-checkout $GIT_BU_URL $KC <&0"
   fi
   
   # Clone from git
   eval $CMD
  
   # Checkout kuali coeus code
-  CMD="cd $KC && git checkout $GIT_REFSPEC"
+  CMD="cd $KC && git checkout $GIT_BU_REFSPEC"
   echo $CMD && eval $CMD
 }
 
@@ -124,6 +138,11 @@ getKuali() {
 setMavenVersions() {
   # Get the content of the pom file with all return/newline characters removed.
   local POM="$1"
+  if [ ! -f $POM ] ; then
+    echo "Cannot find $POM"
+    return 0
+  fi
+
   local content=$(cat ${POM} | sed ':a;N;$!ba;s/\n//g')
 
   # Get versions of dependencies, use a zero width lookbehind for the open element and capture
@@ -141,7 +160,31 @@ setMavenVersions() {
 }
 
 getSchemaSpy() {
-  [ -z "$SCHEMASPY_VERSION" ] && setMavenVersions /opt/kuali/kc/pom.xml
+  local version="$1"
+  if [ -z "$version" ] ; then
+    version="$(getMavenVersion $KC/pom.xml schemaspy)"
+  fi
+  if [ -z "$version" ] ; then
+    echo "Could not resolve schemaspy maven version"
+    return 1
+  fi
+
+  local GIT_KUALICO_HOST="$(propertyFileLookup GIT_KUALICO_HOST)"
+  local GIT_KUALICO_ORG="$(propertyFileLookup GIT_KUALICO_ORG)"
+  local GIT_KUALICO_USER="$(propertyFileLookup GIT_KUALICO_USER)"
+  local GIT_KUALICO_PASSWORD="$(propertyFileLookup GIT_KUALICO_PASSWORD)"
+  local GIT_KUALICO_SCHEMASPY_REPO="$(propertyFileLookup GIT_KUALICO_SCHEMASPY_REPO)"
+  GIT_URL="https://${GIT_KUALICO_USER}@${GIT_KUALICO_HOST}/${GIT_KUALICO_ORG}/${GIT_KUALICO_SCHEMASPY_REPO}.git"
+
+  local CMD="echo \"$GIT_KUALICO_PASSWORD\" | git clone --no-checkout $GIT_URL $BASE/$GIT_KUALICO_SCHEMASPY_REPO <&0"
+  echo "echo \"*******\" | git clone --no-checkout $GIT_BU_URL $BASE/$GIT_KUALICO_SCHEMASPY_REPO <&0"
+  
+  # Clone from git
+  eval $CMD
+ 
+  # Checkout kuali coeus code by tag
+  CMD="cd $KC && git checkout $schemaspy-version"
+  echo $CMD && eval $CMD
 }
 
 getRice() {
