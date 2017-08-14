178f48eb37ce82cdb49b90305696ed58ad5dc696
root <root@ip-10-57-237-90.ec2.internal>
2017-08-14 21:42:15 +0000

    Adding AwardNotice check to maven build

--- aws/ec2-setup-developer/setup.sh
+++ aws/ec2-setup-developer/setup.sh
@@ -28,9 +28,17 @@ run() {
 
   getS2sgen
 
-  configureKuali
+  buildSchemaSpy
+
+  buildRice
+
+  build Api
+
+  buildS2sgen
 
   buildKuali
+
+  configureKuali
 
   runKuali
 }
@@ -78,6 +86,7 @@ installTomcat8() {
 }
 
 getKuali() {
+  HERE="$(pwd)"
   [ -z "$KC" ] && KC=/opt/kuali/kc
   if [ -f $KC/pom.xml ] ; then
     echo "Kuali codebase already exists at $KC"
@@ -102,14 +111,17 @@ getKuali() {
   else
     # Get the user and password. NOTE Assume the git password is an arg passed to this function, 
     # else go to the properties file for it.
-    GIT_BU_USER="$(propertyFileLookup GIT_BU_USER)"
-    GIT_BU_PASSWORD="$1"
-    [ -z "$GIT_BU_PASSWORD" ] && GIT_BU_PASSWORD="$(propertyFileLookup GIT_BU_PASSWORD)"
+    local GIT_BU_USER="$(propertyFileLookup GIT_BU_USER)"
+    local GIT_BU_PASSWORD="$1"
+    if [ -z "$GIT_BU_PASSWORD" ] ; then
+      GIT_BU_PASSWORD="$(propertyFileLookup GIT_BU_PASSWORD)"
+    fi
+    GIT_BU_PASSWORD="$(echo -ne $GIT_BU_PASSWORD | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g')"
 
     # Compose the full git url
     if [ -n "$GIT_BU_USER" ] && [ -n "$GIT_BU_PASSWORD" ] ; then 
       echo "No git private ssh key found. Trying http protocol with username and password"
-      GIT_BU_URL="https://${GIT_BU_USER}@${GIT_BU_HOST}/${GIT_BU_ORG}/${GIT_BU_REPO}.git"
+      GIT_BU_URL="https://${GIT_BU_USER}:${$GIT_BU_PASSWORD}@${GIT_BU_HOST}/${GIT_BU_ORG}/${GIT_BU_REPO}.git"
     else
       GIT_BU_PASSWORD=""
       echo "No git authentication available. Assuming public access ..."
@@ -119,27 +131,29 @@ getKuali() {
 
   if [ -z "$GIT_BU_PASSWORD" ] ; then
     local CMD="git clone --no-checkout $GIT_BU_URL $KC"
-    echo $CMD
   else
-    local CMD="echo \"$GIT_BU_PASSWORD\" | git clone --no-checkout $GIT_BU_URL $KC <&0"
-    echo "echo \"*******\" | git clone --no-checkout $GIT_BU_URL $KC <&0"
+    local CMD="git clone --no-checkout $GIT_BU_URL $KC"
   fi
   
   # Clone from git
-  eval $CMD
+  echo $CMD && eval $CMD
  
   # Checkout kuali coeus code
   CMD="cd $KC && git checkout $GIT_BU_REFSPEC"
   echo $CMD && eval $CMD
+  cd $HERE
 }
 
 # The kuali-research pom file is analyzed here for maven version numbers of each module that needs to be built.
 # The results are output to session variables.
-setMavenVersions() {
+getMavenVersion() {
   # Get the content of the pom file with all return/newline characters removed.
   local POM="$1"
   if [ ! -f $POM ] ; then
-    echo "Cannot find $POM"
+    return 0
+  fi
+  local REPO="$2"
+  if [ -z "$REPO" ] ; then
     return 0
   fi
 
@@ -147,56 +161,152 @@ setMavenVersions() {
 
   # Get versions of dependencies, use a zero width lookbehind for the open element and capture
   # all following characters thereafter until a closing element character is encountered
-
-  SCHEMASPY_VERSION=$(echo "$content" | grep -Po '(?<=<schemaspy\.version>)([^<]+)')
-  RICE_VERSION=$(echo "$content" | grep -Po '(?<=<rice\.version>)([^<]+)')
-  API_VERSION=$(echo "$content" | grep -Po '(?<=<coeus\-api\-all\.version>)([^<]+)')
-  S2SGEN_VERSION=$(echo "$content" | grep -Po '(?<=<coeus\-s2sgen\.version>)([^<]+)')
-
-  echo "SCHEMASPY_VERSION = $SCHEMASPY_VERSION"
-  echo "RICE_VERSION = $RICE_VERSION"
-  echo "API_VERSION = $API_VERSION"
-  echo "S2SGEN_VERSION = $S2SGEN_VERSION"
+  if [ -n "$(echo $REPO | grep -i 'schemaspy')" ] ; then
+    local REGEX='(?<=<schemaspy\.version>)([^<]+)'
+  elif [ -n "$(echo $REPO | grep -i 'rice')" ] ; then
+    local REGEX='(?<=<rice\.version>)([^<]+)'
+  elif [ -n "$(echo $REPO | grep -i 'api')" ] ; then
+    local REGEX='(?<=<coeus\-api\-all\.version>)([^<]+)'
+  elif [ -n "$(echo $REPO | grep -i 's2sgen')" ] ; then
+    local REGEX='(?<=<coeus\-s2sgen\.version>)([^<]+)'
+  fi
+  
+  echo $(echo "$content" | grep -Po $REGEX)
 }
 
 getSchemaSpy() {
-  local version="$1"
-  if [ -z "$version" ] ; then
-    version="$(getMavenVersion $KC/pom.xml schemaspy)"
+  getFromKualiCo GIT_KUALICO_SCHEMASPY_REPO schemaspy
+}
+
+# NOTE: Jenkins builds rice on top of a BU customization. Here we are fetching from git without this.
+getRice() {
+  getFromKualiCo GIT_KUALICO_RICE_REPO rice
+}
+
+getApi() {
+  getFromKualiCo GIT_KUALICO_API_REPO coeus-api
+}
+
+getS2sgen() {
+  getFromKualiCo GIT_KUALICO_S2SGEN_REPO coeus-s2sgen
+}
+
+getFromKualiCo() {
+  local HERE="$(pwd)"
+  local VERSION=""
+  local PROPERTY="$1"
+  local MAVEN_TAG="$2"
+  if [ "$#" -eq 0 ]; then
+    echo "ERROR! Expected 1 or 2 parameters!"
+    return 1
+  elif [ "$#" -eq 1 ]; then
+    VERSION="$1"
+  elif [ "$#" -eq 2 ]; then
+    VERSION="$(getMavenVersion $KC/pom.xml $MAVEN_TAG)"
   fi
-  if [ -z "$version" ] ; then
+  
+  if [ -z "$VERSION" ] ; then
     echo "Could not resolve schemaspy maven version"
     return 1
   fi
 
-  local GIT_KUALICO_HOST="$(propertyFileLookup GIT_KUALICO_HOST)"
-  local GIT_KUALICO_ORG="$(propertyFileLookup GIT_KUALICO_ORG)"
-  local GIT_KUALICO_USER="$(propertyFileLookup GIT_KUALICO_USER)"
-  local GIT_KUALICO_PASSWORD="$(propertyFileLookup GIT_KUALICO_PASSWORD)"
-  local GIT_KUALICO_SCHEMASPY_REPO="$(propertyFileLookup GIT_KUALICO_SCHEMASPY_REPO)"
-  GIT_URL="https://${GIT_KUALICO_USER}@${GIT_KUALICO_HOST}/${GIT_KUALICO_ORG}/${GIT_KUALICO_SCHEMASPY_REPO}.git"
+  local HOST="$(propertyFileLookup GIT_KUALICO_HOST)"
+  local ORG="$(propertyFileLookup GIT_KUALICO_ORG)"
+  local USER="$(propertyFileLookup GIT_KUALICO_USER)"
+  # urlencode every byte of the password
+  local PASSWORD="$(propertyFileLookup GIT_KUALICO_PASSWORD)"
+  PASSWORD="$(echo -ne $PASSWORD | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g')"
+  local REPO="$(propertyFileLookup $PROPERTY)"
+  local URL="https://${USER}:${PASSWORD}@${HOST}/${ORG}/${REPO}.git"
+  local TARGET="$BASE/$REPO"
 
-  local CMD="echo \"$GIT_KUALICO_PASSWORD\" | git clone --no-checkout $GIT_URL $BASE/$GIT_KUALICO_SCHEMASPY_REPO <&0"
-  echo "echo \"*******\" | git clone --no-checkout $GIT_BU_URL $BASE/$GIT_KUALICO_SCHEMASPY_REPO <&0"
-  
-  # Clone from git
-  eval $CMD
- 
-  # Checkout kuali coeus code by tag
-  CMD="cd $KC && git checkout $schemaspy-version"
+  # Clone or fetch from git
+  if [ -d $TARGET ] ; then
+    echo "$TARGET already cloned, fetching instead..."
+    local CMD="cd $TARGET && git fetch --tags $URL"
+  else
+    local CMD="git clone --no-checkout $URL $TARGET"
+  fi  
   echo $CMD && eval $CMD
+
+  # Checkout kuali coeus code by tag
+  echo "$MAVEN_TAG-$VERSION" 
+  if [ -d $TARGET ] ; then
+    CMD="cd $TARGET && git checkout $MAVEN_TAG-$VERSION"
+    echo $CMD && eval $CMD
+  else
+    echo "ERROR! Failed to clone to $TARGET"
+  fi
+  cd $HERE
 }
 
-getRice() {
-  [ -z "$RICE_VERSION" ] && setMavenVersions /opt/kuali/kc/pom.xml
+buildSchemaSpy() {
+  buildModule GIT_KUALICO_SCHEMASPY_REPO
 }
 
-getApi() {
-  [ -z "$API_VERSION" ] && setMavenVersions /opt/kuali/kc/pom.xml
+buildRice() {
+  buildModule GIT_KUALICO_RICE_REPO
 }
 
-getS2sgen() {
-  [ -z "$S2SGEN_VERSION" ] && setMavenVersions /opt/kuali/kc/pom.xml
+buildApi() {
+  buildModule GIT_KUALICO_API_REPO
+}
+
+buildS2sgen() {
+  buildModule GIT_KUALICO_S2SGEN_REPO
+}
+
+buildKuali() {
+  buildModule "kc"
+}
+
+buildModule() {
+  local BUILD_CONTEXT="$(pwd)"
+  if [ $1 == "kc" ] ; then
+    cd $BASE/kc
+    
+    checkAwardNotice "$BUILD_CONTEXT/bu-awardnotice-1.1.jar"
+
+    mvn clean compile package -e -Dgrm.off=true -Dmaven.test.skip=true
+  else
+    local REPO="$(propertyFileLookup $1)"
+    cd $BASE/$REPO    
+    if [ -n "$(echo $1 | grep -i rice)" ] ; then
+      # If the local maven repo does not have the artifacts required to run tests, then you
+      # cannot skip tests in the build. That is, "-Dmaven.test.skip=true" has to be ommitted.
+      # You may, however, be able to skip tests on subsequent builds.
+      mvn clean compile install -e -Dgrm.off=true
+    else
+      mvn clean compile install -e -Dgrm.off=true -Dmaven.test.skip=true
+    fi
+  fi
+  cd $BUILD_CONTEXT
+}
+
+# This is a custom BU jar that has been pre-built. 
+# Making sure it is installed in the local maven repository.
+checkAwardNotice() {
+  local SOURCE_JAR="$1"
+  if [ ! -f $SOURCE_JAR ] ; then
+    "WARNING! $SOURCE_JAR does not exist! - coeus-impl portion of maven build may fail!"
+    return 0
+  fi
+  local GROUP="edu.bu"
+  local ARTIFACTID="bu-awardnotice"
+  local VERSION="1.1"
+  local ARTIFACT="$GROUP:$ARTIFACTID:$VERSION"
+  local LOCALREPO="file://~/.m2/repository"
+
+  local AWARDNOTICE=mvn dependency:get -Dartifact=$ARTIFACT -o -DrepoUrl=$LOCALREPO | grep 'BUILD SUCCESS'
+
+  if [ -z "$AWARDNOTICE" ] ; then
+    mvn install:install-file \
+      -Dfile=$SOURCE_JAR \
+      -DgroupId=$GROUP \
+      -DartifactId=$ARTIFACTID \
+      -Dversion=$VERSION \
+      -Dpackaging=jar
+  fi
 }
 
 configureKuali() {
@@ -220,10 +330,6 @@ configureKuali() {
   sed -i "s/DB_HOST/$DB_HOST/g" kc-config.xml
   sed -i "s/DB_SERVICE_NAME/$DB_SERVICE_NAME/g" kc-config.xml
   sed -i "s/DB_SCHEMA/$DB_SCHEMA/g" kc-config.xml  
-}
-
-buildKuali() {
-  echo "NOT DONE"
 }
 
 runKuali() {
